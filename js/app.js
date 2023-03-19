f3e23ec87920dcbb413c9d8691c2c9a084429804
fdomoreno <fdomoreno.ingeniero@gmail.com>
2023-03-19 14:32:28 -0500

    Se ajusta ruteador y estilos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-    console.log("Se ejecuta inicialmente");
+$(document).ready(function() {
+    $("#content").load("./pages/inicio.html");
+
+    $("#navbarSupportedContent a").click(function(event) {
+        var item = event.target.id
+        if(item.trim()!=""){
+            var enlace = item.trim().split("-");
+            $("#content").load("./pages/"+enlace[1]+".html", function(responseText, statusText, xhr){
+                if (statusText == "error" ) {
+                    $("#content").load("./pages/404.html");
+                }
+            })
+        }
+    });
 });
